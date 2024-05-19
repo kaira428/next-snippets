ddb86dd1bcd71fbdeac1114fc616b3335788b3f8
Wong Keng <kaira428@gmail.com>
2024-05-19 16:00:24 +0800

    Init commit. Add Creation Form

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const SnippetCreate = () => {
+    return (
+        <form>
+            <h3 className='font-bold m-3'>Create a Snippet</h3>
+            <div className='flex flex-col gap-4'>
+                <div className='flex gap-4'>
+                    <label className='w-12' htmlFor='title'>Title</label>
+                    <input
+                        name="title"
+                        className='border rounded p-2 w-full'
+                        id="title" />
+                </div>
+                <div className='flex gap-4'>
+                    <label className='w-12' htmlFor='code'>Code</label>
+                    <textarea
+                        name="code"
+                        className='border rounded p-2 w-full'
+                        id="code" />
+                </div>
+
+                <button type="submit" className='rounded p-2 bg-blue-200 hover:cursor-pointer hover:bg-blue-400 hover:text-white'>Create</button>
+            </div>
+        </form>
+    )
+}
+
+export default SnippetCreate
